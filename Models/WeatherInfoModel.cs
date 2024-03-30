5912b4a28c70707eab76f7a83bd43c32b6cf6938
Defender0345 <dylannel02@gmail.com>
2024-03-30 16:42:19 +0200

    Add project files.

---
+++ Models/WeatherInfoModel.cs
@@ -0,0 +1,38 @@
+﻿namespace CSWeatherApp.Models
+{
+    public class WeatherInfoModel
+    {
+        public Weather[] weather { get; set; }
+        public Main main { get; set; }
+        public Wind wind { get; set; }
+        public Sys sys { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Main
+    {
+        public float temp { get; set; }
+        public float feels_like { get; set; }
+        public float temp_min { get; set; }
+        public float temp_max { get; set; }
+    }
+
+    public class Wind
+    {
+        public float speed { get; set; }
+    }
+
+    public class Sys
+    {
+        public string country { get; set; }
+        public int sunrise { get; set; }
+        public int sunset { get; set; }
+    }
+
+    public class Weather
+    {
+        public string main { get; set; }
+        public string description { get; set; }
+    }
+
+}
